139213e43b622ed935a76e1d560265855fa6c8f4
mohamed ahmed <105215559+mohanjrme@users.noreply.github.com>
2022-07-01 10:06:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// loop over an array of names
+
+let names = ["james", "justin", "john"];
+
+for (i = 0; i < names.length; i++)
+console.log("hello " + names[i]);
+
+let names2 = ["james", "justin", "john"];
+
+for (i = 0; i < names.length; i++)
+console.log("Goodbye " + names[i]);
